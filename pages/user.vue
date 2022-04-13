b5d3234197236abdc02f7a225a299702589cde9a
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-14 00:45:47 +0430

    using reusable component in parent and show it to all pages related to that path

---
+++ pages/user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- this part is going to run in all user pages -->
+    <div>The Root of the user pages</div>
+
+    <!-- the user pages are showing in here -->
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
